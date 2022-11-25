e5fe6ec486572d2d01e4a1bdb7c2d5da634d6719
Sylvia Boamah <60356081+sboamah@users.noreply.github.com>
2022-11-25 18:56:13 -0500

    More pages

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -11,7 +11,7 @@ export const PROJECTS: Projects[] = [
     {
         id: 1,
         title: 'ReCollect',
-        img: 'assets/recollect_logo.png',
+        img: 'assets/ReCollectBanner.png',
         role: 'Mobile UI/Front-End Developer',
         description: 'Developed a mobile application for users with memory-related ailments to connect with family.',
         key: 'recollect'
@@ -20,7 +20,7 @@ export const PROJECTS: Projects[] = [
     {
         id: 2,
         title: 'Project Shadow',
-        img: 'assets/ps_logo.png',
+        img: 'assets/PSBanner.png',
         role: 'Mobile UI Developer Intern',
         description: 'Collaborated to integrate screens and finalize user workflows for an iOS and Android mobile stock trading application.',
         key: 'project-shadow'
@@ -28,7 +28,7 @@ export const PROJECTS: Projects[] = [
     {
         id: 3,
         title: 'Launch Hoboken Design Competition',
-        img: 'assets/LH_LOGO.png',
+        img: 'assets/LHBanner.png',
         role: 'UI Design Finalist',
         description: "Created a website mockup for Launch Hoboken's online Innovation Portal; received second place.",
         key: 'launch-hoboken'
@@ -36,7 +36,7 @@ export const PROJECTS: Projects[] = [
     {
         id: 4,
         title: 'Positive Affirmation Generator',
-        img: 'assets/PAGseconddesign.jpg',
+        img: 'assets/PAGBanner.png',
         role: 'UI Designer and Developer',
         description: "Designed and implemented a responsive, user-friendly interface to generate and display randomized affirming sentences.",
         key: 'positive-affirmation-generator'
